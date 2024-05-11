91a3fe9a1a7c08797cb1d944edfc4422fdbc0df3
jwt2706 <jwt2home@gmail.com>
2024-05-10 21:35:19 -0400

    server error catching

--- api/upload.js
+++ api/upload.js
@@ -4,31 +4,36 @@ const nbt = require("prismarine-nbt");
 const fs = require("fs");
 
 module.exports = (req, res) => {
-  upload.single("file")(req, res, function (err) {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-
-    // req.file is the 'file' file
-    // req.body will hold the text fields, if there were any
-    console.log(req.file);
-
-    // Parse schematic file here
-    fs.readFile(req.file.path, function (error, data) {
-      if (error) {
-        return res.status(500).json({ error: error.message });
+  try {
+    upload.single("file")(req, res, function (err) {
+      if (err) {
+        return res.status(500).json({ error: err.message });
       }
 
-      nbt.parse(data, function (error, data) {
+      // req.file is the 'file' file
+      // req.body will hold the text fields, if there were any
+      console.log(req.file);
+
+      // Parse schematic file here
+      fs.readFile(req.file.path, function (error, data) {
         if (error) {
           return res.status(500).json({ error: error.message });
         }
 
-        console.log(data.value); // This will log the parsed NBT data
+        nbt.parse(data, function (error, data) {
+          if (error) {
+            return res.status(500).json({ error: error.message });
+          }
 
-        // Send a response back to the client
-        res.json({ message: "File received", data: data.value });
+          console.log(data.value); // This will log the parsed NBT data
+
+          // Send a response back to the client
+          res.json({ message: "File received", data: data.value });
+        });
       });
     });
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 };
